5758bba8e7c0e64103f7ca8de299db3b6128d1bc
Govind Rawal <govind.raval@neogeninfotech.com>
2024-07-09 19:02:37 +0530

    added tailwind and props

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from '../component/card'
 function App() {
-  const [counter, setCounter] = useState(0)
 
+
+  const [counter, setCounter] = useState(0)
+  let myObj = [1, 2, 3]
   const addCounter = () => {
     if (counter + 1 > 20) {
       alert('Limit exceeded')
     } else {
-      setCounter(counter + 1)
+      setCounter(vcounter => vcounter + 1)
+
+
     }
     ;
   }
@@ -31,6 +35,13 @@ function App() {
 
       <button onClick={addCounter}>Add Counter {counter}</button>
       <button onClick={removeCounter}>Remove Counter {counter}</button>
+
+
+      <h1 className="text-3xl font-bold underline mb-4">
+        Hello world!
+      </h1>
+      <Card username="GovindIt" btnText="Read More" />
+
 
     </>
   )
